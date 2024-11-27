81de5493d0755d8872950ba06c00643c70b5a245
Arijit2003 <arijitmodak2003@gmail.com>
2024-11-27 21:27:53 +0530

    first-commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/userRoutes')
+const messagesRoute = require('./routes/messagesRoute')
+const socket = require('socket.io')
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors())//open cors.
+app.use(express.json())
+app.use("/api/auth",userRoutes)
+app.use("/api/messages",messagesRoute)
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{ 
+    console.log("MongoDB coneected successfully")
+}).catch((err)=>{
+    console.log(err.message)
+})
+
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server started on PORT: ${process.env.PORT}`)
+})
+
+const io = socket(server,{
+    cors:{
+        origin: "http://localhost:5173",
+        credentials: true,
+    }
+})
+global.onlineUsers = new Map()
+io.on("connection",(socket)=>{
+    global.chatSocket=socket
+    socket.on("add-user",(userId)=>{
+        onlineUsers.set(userId,socket.id)
+    })
+    socket.on("send-msg",(data)=>{
+        console.log(data)
+        const sendUserSocket = onlineUsers.get(data.to)
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("msg-receive",data.message)
+        }
+    })
+})
